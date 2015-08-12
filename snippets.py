d2095482a08ca5e008a4cebc455a3dc27c2c42e5
thomasambiz <thomasambiz@gmail.com>
2015-08-12 00:47:08 +0000

    Add parser structure

--- snippets.py
+++ snippets.py
@@ -26,7 +26,26 @@ def main():
     """Main function"""
     logging.info("Contruction parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_arguments("name", help="The name of the snippet")
+    put_parser.add_arguments("nippet", help=("The snippet text"))
+    
     arguments = parser.parse_args(sys.argv[1:])
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+    
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippets = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
     
 if __name__ == "__main__":
     main()
